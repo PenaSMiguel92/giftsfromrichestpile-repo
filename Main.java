bd59c01b79db21fcd561985e0aed3422ff41ea53
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 14:44:08 -0700

    initial

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.pickGifts(new int[] { 25, 64, 9, 4, 100 }, 4));
+        System.out.println(sol.pickGifts(new int[] { 1, 1, 1, 1 }, 4));
+    }
+}
